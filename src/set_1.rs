4a5b1972578411e8d4218931b7bbb24dd9a14cf4
Arun Kulshreshtha <kulshrax@gmail.com>
2016-12-20 23:08:00 -0800

    Fix linebreak escaping.

--- src/set_1.rs
+++ src/set_1.rs
@@ -160,15 +160,15 @@ mod tests {
         let (index, result) = challenge_8();
 
         // Note that the second 16-byte chunk is repeated 4 times.
-        let expected = "d880619740a8a19b7840a8a31c810a3d/
-                        08649af70dc06f4fd5d2d69c744cd283/
-                        e2dd052f6b641dbf9d11b0348542bb57/
-                        08649af70dc06f4fd5d2d69c744cd283/
-                        9475c9dfdbc1d46597949d9c7e82bf5a/
-                        08649af70dc06f4fd5d2d69c744cd283/
-                        97a93eab8d6aecd566489154789a6b03/
-                        08649af70dc06f4fd5d2d69c744cd283/
-                        d403180c98c8f6db1f2a3f9c4040deb0/
+        let expected = "d880619740a8a19b7840a8a31c810a3d\
+                        08649af70dc06f4fd5d2d69c744cd283\
+                        e2dd052f6b641dbf9d11b0348542bb57\
+                        08649af70dc06f4fd5d2d69c744cd283\
+                        9475c9dfdbc1d46597949d9c7e82bf5a\
+                        08649af70dc06f4fd5d2d69c744cd283\
+                        97a93eab8d6aecd566489154789a6b03\
+                        08649af70dc06f4fd5d2d69c744cd283\
+                        d403180c98c8f6db1f2a3f9c4040deb0\
                         ab51b29933f2c123c58386b06fba186a";
 
         assert_eq!(result, expected);
