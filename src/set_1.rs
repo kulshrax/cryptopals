e38a189ef054418f9e49d637316f407bd391ce60
Arun Kulshreshtha <kulshrax@gmail.com>
2016-12-20 23:05:15 -0800

    Format ECB ciphertext string into 16-byte lines for clarity.

--- src/set_1.rs
+++ src/set_1.rs
@@ -158,11 +158,19 @@ mod tests {
     #[test]
     fn text_challenge_8() {
         let (index, result) = challenge_8();
-        let expected = "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e\
-                        2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd28394\
-                        75c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a\
-                        93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403\
-                        180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a";
+
+        // Note that the second 16-byte chunk is repeated 4 times.
+        let expected = "d880619740a8a19b7840a8a31c810a3d/
+                        08649af70dc06f4fd5d2d69c744cd283/
+                        e2dd052f6b641dbf9d11b0348542bb57/
+                        08649af70dc06f4fd5d2d69c744cd283/
+                        9475c9dfdbc1d46597949d9c7e82bf5a/
+                        08649af70dc06f4fd5d2d69c744cd283/
+                        97a93eab8d6aecd566489154789a6b03/
+                        08649af70dc06f4fd5d2d69c744cd283/
+                        d403180c98c8f6db1f2a3f9c4040deb0/
+                        ab51b29933f2c123c58386b06fba186a";
+
         assert_eq!(result, expected);
         assert_eq!(index, 132);
     }
